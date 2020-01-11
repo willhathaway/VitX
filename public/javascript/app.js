fec697d4113eb3f5d98f520fdca457eaadcb12c2
Will Hathaway <william.hathaway38@gmail.com>
2020-01-11 12:44:29 -0500

    public js files deleted. basic form testing in progress

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,41 +1,75 @@
-$('#view-btn').on("click", function (event) {
-    event.preventDefault();
+$(document).ready(function () {
+    console.log('javascript loaded')
 
-    $.get("api/workouts", function (data) {
+    $('#view-btn').on('click', function (event) {
+        event.preventDefault();
 
-        // for each character that our server sends us back
-        for (let i = 0; i < data.length; i++) {
-            // create a parent div for the oncoming elements
-            let workoutData = $("<div>");
-            // add a class to this div
-            workoutData.addClass("workoutData");
-            // add an id to the well to mark 
-            workoutData.attr("id", "workoutData" + i);
-            // append the div to data div
-            $("#data").append(workoutData);
+        console.log('view');
 
-            // Now add all of our data to the div we just placed on the page
+        $.get("api/workouts", function (data) {
 
-        }
+            // for each character that our server sends us back
+            for (let i = 0; i < data.length; i++) {
+                // create a parent div for the oncoming elements
+                let workoutData = $("<div>");
+                // add a class to this div
+                workoutData.addClass("workoutData");
+                // add an id to the well to mark 
+                workoutData.attr("id", "workoutData" + i);
+                // append the div to data div
+                $("#data").append(workoutData);
+
+                // get data from workouts data
+
+            }
+        });
+
+        $.get("api/nutrition", function (data) {
+
+            // for each food that our server sends us back
+
+            for (let i = 0; i < data.length; i++) {
+                // create a parent div for the oncoming elements
+                let nutritionData = $("<div>");
+                // add a class to this div
+                nutritionData.addClass("nutritionData");
+                // add an id to the well to mark 
+                nutritionData.attr("id", "nutritionData" + i);
+                // append the div to data div
+                $("#data").append(nutritionData);
+
+                // get data from nutrition table
+
+            }
+        });
     });
 
+    $('#nutrition-btn').on('click', function (event) {
 
-    $.get("api/nutrition", function (data) {
+        event.preventDefault();
 
-        // for each food that our server sends us back
+        console.log('nutrition');
 
-        for (let i = 0; i < data.length; i++) {
-            // create a parent div for the oncoming elements
-            let nutritionData = $("<div>");
-            // add a class to this div
-            nutritionData.addClass("nutritionData");
-            // add an id to the well to mark 
-            nutritionData.attr("id", "nutritionData" + i);
-            // append the div to data div
-            $("#data").append(nutritionData);
+        let data = $('#post');
 
-            // Now add all of our data to the div we just placed on the page
+        console.log(data);
 
-        }
+        // post data to nutrition
+
     });
+
+    $('#workouts-btn').on('click', function (event) {
+
+        event.preventDefault();
+
+        console.log('workouts');
+
+        let data = $('#post');
+
+        console.log(data);
+
+        // post data to workouts
+
+    });
+
 });
