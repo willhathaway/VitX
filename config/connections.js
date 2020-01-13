c88f49b54df2af644df732fe4dd67ecd54d82251
Will Hathaway <william.hathaway38@gmail.com>
2020-01-13 15:29:38 -0500

    front-end jQuery form/API search done, except for POST request to server.

--- config/connections.js
+++ config/connections.js
@@ -9,10 +9,10 @@ var mysql = require("mysql");
 const source = {
   localhost: {
     host: "localhost",
-    port: 3306,
+    port: 1542,
     user: "root",
     password: "password",
-    database: "db"
+    database: "blazeDB"
   }
 };
 
--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,10 +1,144 @@
 $(document).ready(function () {
-    console.log('javascript loaded')
 
-    $('#view-btn').on('click', function (event) {
+    console.log("javascript loaded")
+
+    // for searching:
+
+    // fitbit API
+
+    $("#workouts-btn").on("click", function () {
+
+        let exersize = $("#search-exersizes")[0].elements[0].value;
+
+        console.log(exersize);
+
+        $.get("api/exersizes", function (data) {
+            let results = [];
+            for (let i = 0; i < data.length; i++) {
+                if (data[i].name.includes(exersize)) {
+                    results.push(data[i]);
+                }
+            }
+            console.log(results);
+
+            for (let i = 0; i < results.length; i++) {
+
+                let options = $("<button id=" + results[i].id + ">");
+                options.html(results[i].name);
+                options.attr("class", "exersize")
+                options.appendTo($("#data"));
+
+            }
+
+        });
+
+    })
+
+
+    // edemam API
+
+    let foodOptions = [];
+
+    $("#nutrition-btn").on("click", function () {
+
+        let food = "apple";
+
+        let app_id = "319d5cf3";
+
+        let app_key = "a6b7b2b30443063fa2630ff0dbc8f735"
+
+        let queryURL = "https://api.edamam.com/api/food-database/parser?limit=5&app_id=" + app_id + "&app_key=" + app_key + "&ingr=" + food;
+
+        $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+
+            // Function is .then() called with the response object:
+
+            .then(function (response) {
+
+                // Console logs the response to see what the object looks like, how to retrieve information:
+
+                console.log(response);
+
+                for (let i = 0; i < response.hints.length; i++) {
+                    foodOptions.push(response.hints[i]);
+                    let options = $("<button id=" + response.hints[i].food.label + ">");
+                    options.html(response.hints[i].food.label);
+                    options.attr("class", "nutrition");
+                    options.attr("id", response.hints[i].food.foodId)
+                    options.appendTo($("#data"));
+
+                }
+
+            });
+
+    });
+
+    // for adding items to submit to server:
+
+    $(document).on("click", ".exersize", function () {
+
+        console.log("add exersize to post form");
+
+        let thisID = this.id;
+
+        $.get("api/exersizes", function (data) {
+            let results = [];
+
+            for (let i = 0; i < data.length; i++) {
+
+                if (data[i].id === thisID) {
+                    results.push(data[i]);
+                }
+            }
+
+            for (let i = 0; i < results.length; i++) {
+                let postExersize = $("<p>");
+                $(postExersize).html(results[i].name);
+                $("#exersizes-post").append(postExersize);
+            }
+
+        });
+
+
+    })
+
+    $(document).on("click", ".nutrition", function () {
+
+        console.log("add nutrition to post form");
+
+        let thisID = this.id;
+
+        console.log(foodOptions);
+
+        console.log(thisID);
+        console.log(foodOptions[0].food.foodId);
+
+        for (let i = 0; i < foodOptions.length; i++) {
+            
+            if (foodOptions[i].food.foodId === thisID) {
+                let postNutrition = $("<p>");
+                $(postNutrition).html(foodOptions[i].food.label);
+                $("#nutrition-post").append(postNutrition);
+            }
+        }
+
+    })
+
+
+
+
+
+    // for viewing saved user data:
+
+
+    $("#view-btn").on("click", function (event) {
+
         event.preventDefault();
 
-        console.log('view');
+        console.log("view");
 
         $.get("api/workouts", function (data) {
 
@@ -44,32 +178,34 @@ $(document).ready(function () {
         });
     });
 
-    $('#nutrition-btn').on('click', function (event) {
+    // for posting:
 
-        event.preventDefault();
+    // $("#nutrition-btn").on("click", function (event) {
 
-        console.log('nutrition');
+    //     event.preventDefault();
 
-        let data = $('#post');
+    //     console.log("nutrition");
 
-        console.log(data);
+    //     let data = $("#post");
 
-        // post data to nutrition
+    //     console.log(data);
 
-    });
+    //     // post data to nutrition
 
-    $('#workouts-btn').on('click', function (event) {
+    // });
 
-        event.preventDefault();
+    // $("#workouts-btn").on("click", function (event) {
 
-        console.log('workouts');
+    //     event.preventDefault();
 
-        let data = $('#post');
+    //     console.log("workouts");
 
-        console.log(data);
+    //     let data = $("#post");
 
-        // post data to workouts
+    //     console.log(data);
 
-    });
+    //     // post data to workouts
+
+    // });
 
 });
