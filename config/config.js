fe89ad04a4bcca2f74365dd6a94e6bf36aba8de6
Will Hathaway <william.hathaway38@gmail.com>
2020-01-16 21:22:35 -0500

    changed config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "password",
+    "password": process.env.password,
     "database": "blazedb",
     "host": "127.0.0.1",
     "dialect": "mysql"
